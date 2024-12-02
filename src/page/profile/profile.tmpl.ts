6c123cf0afeb847f80ef9da3a6e8468ed863ca04
Dmitry Bolshakov <60438778+Soulaction@users.noreply.github.com>
2024-12-02 21:42:12 +0300

    sprint_1: feat: заготовка под страницу профиля

--- src/page/profile/profile.tmpl.ts
+++ src/page/profile/profile.tmpl.ts
@@ -1,5 +1,49 @@
 export default `
-<main class="page-wrapper">
-
+<main class="page-profile">
+    <div class="left-panel">
+        <button class="left-panel-button">
+        
+        </button>
+    </div>
+    <div class="page-wrapper page-profile-content">
+        <img class="user-avatar" src="../../assets/images/profile.png" alt="Иконка профиля"/>
+        <h1 class="user-name">Дмитрий</h1>
+        <div class="user-info">
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Почта</span>
+                <p class="user-info-description user-info-text">pochta@yandex.ru</p>
+            </div>
+            <div class="user-info-border"></div>
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Логин</span>
+                <p class="user-info-description user-info-text">dmitry</p>
+            </div>
+            <div class="user-info-border"></div>
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Имя</span>
+                <p class="user-info-description user-info-text">Иван</p>
+            </div>
+            <div class="user-info-border"></div>
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Фамилия</span>
+                <p class="user-info-description user-info-text">Иванов</p>
+            </div>
+            <div class="user-info-border"></div>
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Имя в чате</span>
+                <p class="user-info-description user-info-text">Дмитрий</p>
+            </div>
+            <div class="user-info-border"></div>
+            <div class="user-info-item">
+                <span class="user-info-label user-info-text">Телефон</span>
+                <p class="user-info-description user-info-text">+7 (909) 967 30 30</p>
+            </div>
+            <footer class="user-info-footer">
+            {{> Link label="Изменить данные"}}
+            {{> Link label="Изменить пароль"}}
+            {{> Link label="Выйти"}}
+            </footer>
+        </div>
+    </div>
 </main>
 `;
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        port: 5000,
+        port: 3000,
     },
     css: {
         postcss: './postcss.config.js',
