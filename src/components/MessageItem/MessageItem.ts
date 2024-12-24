18951878c8c9afaf651815c84c2373f0e49679af
Dmitry Bolshakov <60438778+Soulaction@users.noreply.github.com>
2024-12-24 22:51:29 +0300

    sprint_2: feat: стили сообщений без галочки прочитано

--- src/components/MessageItem/MessageItem.ts
+++ src/components/MessageItem/MessageItem.ts
@@ -19,17 +19,16 @@ export class MessageItem extends Block {
 
     override render(): string {
         return `<div class="${s.message} {{#if meMessage}}${s.messageMe}{{/if}}">
+                    {{#if newDay}}
+                        <h2 class="${s.dataMsg}">{{newDay}}</h2>
+                    {{/if}}
                     {{#if linkImg}}
                         <div class="${s.messageImgBlock}">
                             <img class="${s.messageImg}" src="{{linkImg}}" alt="Сообщение с картинкой"/>
-                            <span class="${s.messageImgDate}">{{dateMsg}}</span>
+                            <span class="${s.textDate} ${s.messageImgDate}">{{dateMsg}}</span>
                         </div>
                     {{else}}
-                        <p class="${s.messageText} {{#if meMessage}}${s.messageTextMe}{{/if}}">
-                            {{text}}
-                            <span class="${s.messageTextDate}">{{dateMsg}}</span>
-                            dsdnsldvlskdnvksldvnkslvs
-                        </p>
+                        <p class="${s.messageText} {{#if meMessage}}${s.messageTextMe}{{/if}}">{{text}}<span class="${s.textDate} {{#if meMessage}}${s.messageTextDateMe}{{/if}}">{{dateMsg}}</span></p>
                     {{/if}}
                 </div>`;
     }
